718717b94f535808fa14f57eb2ee4b7a8ace9401
Tscharana <171447830+Tscharana@users.noreply.github.com>
2026-07-08 17:52:33 +0200

    feat: add Book constructor and addBookToLibrary function with unique IDs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("JS linked.");
+const myLibrary = [];
+
+function Book(name, author, year, pages) {
+    this.id = crypto.randomUUID();
+    this.name = name;
+    this.author = author;
+    this.year = year;
+    this.pages = pages;
+}
+
+function addBookToLibrary(name, author, year, pages) {
+    const newBook = new Book(name, author, year, pages);
+    myLibrary.push(newBook);
+}
