9ffa87fc4fe488c900717c980c54985d53c591e4
Tscharana <171447830+Tscharana@users.noreply.github.com>
2026-07-10 11:21:46 +0200

    feat: implement display function with card layout and container clearing

--- js/script.js
+++ js/script.js
@@ -11,13 +11,34 @@ function Book(name, author, year, pages) {
 function addBookToLibrary(name, author, year, pages) {
     const newBook = new Book(name, author, year, pages);
     myLibrary.push(newBook);
+    clear();
+    display();
 }
 
-function display(books) {
-    books.forEach(book => {
-        console.log(book.name);
-        console.log(book.author);
-        console.log(book.year);
-        console.log(book.pages);
-        })
+function clear() {
+    while (libraryContainer.firstChild) {
+        libraryContainer.removeChild(libraryContainer.firstChild);
+    }
 }
+
+function display() {
+    myLibrary.forEach((book) => { 
+        const newBook = document.createElement("div");
+        newBook.classList.add(book.id, "card");
+        const name = document.createElement("div");
+        name.textContent = book.name;
+        newBook.appendChild(name);
+        const author = document.createElement("div");
+        author.textContent = book.author;
+        newBook.appendChild(author);
+        const year = document.createElement("div");
+        year.textContent = book.year;
+        newBook.appendChild(year);
+        const pages = document.createElement("div");
+        pages.textContent = book.pages;
+        newBook.appendChild(pages);
+        libraryContainer.appendChild(newBook);
+    })
+}
+
+const libraryContainer = document.querySelector("#library-container");
