6232fa344aa9f551215435ff67e6fb5f1f4d7fd6
Tscharana <171447830+Tscharana@users.noreply.github.com>
2026-07-10 13:15:21 +0200

    feat: implement read boolean

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 const myLibrary = [];
 
-function Book(name, author, year, pages) {
+function Book(name, author, year, pages, read) {
     this.id = crypto.randomUUID();
     this.name = name;
     this.author = author;
     this.year = year;
     this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary(name, author, year, pages) {
-    const newBook = new Book(name, author, year, pages);
+function addBookToLibrary(name, author, year, pages, read) {
+    const newBook = new Book(name, author, year, pages, read);
     myLibrary.push(newBook);
     clear();
     display();
@@ -26,17 +27,20 @@ function display() {
         const newBook = document.createElement("div");
         newBook.classList.add(book.id, "card");
         const name = document.createElement("div");
-        name.textContent = book.name;
+        name.textContent = "\"" + book.name + "\"";
         newBook.appendChild(name);
         const author = document.createElement("div");
-        author.textContent = book.author;
+        author.textContent = "written by " + book.author;
         newBook.appendChild(author);
         const year = document.createElement("div");
-        year.textContent = book.year;
+        year.textContent = "in " + book.year;
         newBook.appendChild(year);
         const pages = document.createElement("div");
-        pages.textContent = book.pages;
+        pages.textContent = book.pages + " pages";
         newBook.appendChild(pages);
+        const read = document.createElement("div");
+        read.textContent = book.read ? "already read" : "not read yet";
+        newBook.appendChild(read);
         libraryContainer.appendChild(newBook);
     })
 }
